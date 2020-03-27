5570d5c4e89444866e2803b71178d49bd99f6e7b
Mohanprakash Khatri <kmohan1231@outlook.com>
2020-03-27 19:18:37 +0545

    class added

---
+++ GPL_Application_2020/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GPL_Application_2020
+{
+    public class Circle:Shape
+    {
+        private float rad;
+        public void GetValue(float width, float height, float hypotenus, float radius)
+        {
+            rad = radius;
+        }
+        public void Draw(Graphics g, int x, int y)
+        {
+            //SolidBrush sb = new SolidBrush(Color.Black);
+            g.DrawEllipse(new Pen(Color.Black, 5), x, y, rad, rad);
+            //g.FillEllipse(sb, 100, 100, 100, 100);// int.Parse(txtshapesize.Text), int.Parse(txtshapesize.Text));
+        }
+
+    }
+}
---
+++ GPL_Application_2020/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GPL_Application_2020
+{
+    public interface Shape
+    {
+        void Draw(Graphics g, int x, int y);
+        void GetValue(float width, float height, float hypotenus, float radius);
+    }
+}
